ef07fbc5c34760b4809f3799d8e79a4c9ec45975
lihaoran <toyhaoran@gmail.com>
2019-02-27 10:05:15 +0800

    init

---
+++ src/main/java/com/haoran/HelloController.java
@@ -0,0 +1,19 @@
+package com.haoran;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: lihaoran
+ * Date: 2019/2/27
+ * Time: 9:56
+ * Description:
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
